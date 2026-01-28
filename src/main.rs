9bea3202e284a0d23ddab1cb0018469caa6cc682
soldwyn <roman.schmucker@icloud.com>
2026-01-28 21:21:27 +0100

    added music

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::rc::Rc; //mutability for "Game" while using Rc
 
 use catan_rust::backend::game::Game;
 use catan_rust::frontend::bevy::FrontendPlugin;
+use catan_rust::audio;
 
 fn main() {
     //building a bevy app, creating the game state and registering the frontend plugin
@@ -17,5 +18,6 @@ fn main() {
         //inserting the game state as a "non-send resource" into bevy so that systems can borrow it on the main thread
         .insert_non_send_resource(game.clone())
         .add_plugins(FrontendPlugin)
+        .add_systems(Startup, audio::play_background_music)
         .run();
 }
